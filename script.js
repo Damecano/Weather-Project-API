e304aee8367a3f46dc9a974cecb5cb5ff591d213
Abdulloh <abdullohcoder27@gmail.com>
2023-11-30 15:32:58 +0500

    Level Up

--- script.js
+++ script.js
@@ -4,54 +4,109 @@ let searchBtn = document.querySelector("#searchBtn");
 let weatherBox = document.querySelector(".weather-box");
 let error404 = document.querySelector(".not-found");
 let weatherDetails = document.querySelector(".weather-details");
-
 let apiKey = "922bcbf1125dc39bad94895c98a2d448";
 let lat, lon;
-
-let part = 'hourly';
-
-getLocation()
-
-
+let part = "hourly";
+getLocation();
+// get location me
 function getLocation() {
-    try{
-        navigator.geolocation.getCurrentPosition(ShowPosition);
-    } catch {
-        return;
-    }
+	try {
+		navigator.geolocation.getCurrentPosition(ShowPosition);
+	} catch {
+		return;
+	}
 }
 
-function ShowPosition(position){
-    lat = position.coords.latitude;
-    lon = position.coords.longitude;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
-        .then(response => response.json())
-        .then(json => {
-            getWeather(json[0].name)
-        });
+// get latitude and longitude in location
+function ShowPosition(position) {
+	lat = position.coords.latitude;
+	lon = position.coords.longitude;
+	fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+		.then(response => response.json())
+		.then(json => {
+			getWeather(json[0].name);
+		});
 }
 
+// if in click search btn
 searchBtn.addEventListener("click", calcWeather);
 
+// if in Enter your Keyboard
 document.addEventListener("keydown", function(event) {
-    if (event.key == "Enter") {
-        calcWeather()
-    }
+	if (event.key == "Enter") {
+		calcWeather()
+	}
 })
 
+// function Calc Weather
 function calcWeather() {
-    let cityTarget = document.getElementById('locationName').value;
-
-    if (cityTarget.trim() == "") {
-        return;
-    } else {
-        getWeather(cityTarget);
-    }
+	let cityTarget = document.getElementById("locationName").value;
+	if (cityTarget.trim() == '') {
+		return;
+	} else {
+		getWeather(cityTarget);
+	}
 }
 
+// getWeather function
+function getWeather(city) {
 
-function getWeather(city){
+	weatherBox.classList.remove("fadeIn");
+	weatherDetails.classList.remove("fadeIn");
+	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+		.then(response => response.json())
+		.then(json => {
+			let imageWeather = document.querySelector(".weather-box img");
+			let cityTitle = document.querySelector(".weather-box .cityName");
+			let temperature = document.querySelector(".weather-box .temperature");
+			let description = document.querySelector(".weather-box .description");
+			let humidity = document.querySelector(".weather-details .humidity span");
+			let wind = document.querySelector(".weather-details .wind span");
+			error404.style.display = "none";
+			error404.classList.remove("fadeIn");
+			if (json.cod == "404") {
+				console.log("error")
+				container.style.height = "650px";
+				weatherBox.style.display = "none";
+				weatherDetails.style.display = "none";
+				error404.style.display = "block";
+				error404.classList.add("fadeIn");
+			} else {
+				switch (json.weather[0].main) {
+					case 'Clear':
+						imageWeather.src = 'img/clear.png';
+						break;
 
-    weatherBox.classList.remove("fadeIn")
-    weatherDetails.classList.remove("fadeIn")
+					case 'Rain':
+						imageWeather.src = 'img/rain.png';
+						break;
+
+					case 'Snow':
+						imageWeather.src = 'img/snow.png';
+						break;
+
+					case 'Clouds':
+						imageWeather.src = 'img/cloud.png';
+						break;
+
+					case 'Haze':
+						imageWeather.src = 'img/mist.png';
+						break;
+
+					default:
+						imageWeather.src = '';
+				}
+				cityTitle.innerHTML = city;
+				temperature.innerHTML = `${parseInt(json.main.temp)}<span>*C</span>`
+				description.innerHTML = json.weather[0].description;
+				humidity.innerHTML = `${json.main.humidity}%`;
+				wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+				container.style.height = "650px"
+				weatherBox.style.display = "";
+				weatherBox.classList.add("fadeIn");
+				weatherDetails.style.display = "flex";
+				weatherDetails.classList.add("fadeIn");
+				document.getElementById("locationName").value = "";
+			}
+		});
 }
